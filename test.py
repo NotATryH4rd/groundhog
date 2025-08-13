79c0199345edbf3f1a776b1995292ce46d99fede
NotATryH4rd <atharvdua09@gmail.com>
2025-08-13 11:20:23 -0400

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ url = "https://groundhog.pythonanywhere.com/predict"
 
 data = {
     "Temperature": 25,
-    "Moisture": 40,
+    "Humidity": 40,
     "ph": 6.5,
     "EC": 0.8
 }
@@ -15,3 +15,4 @@ if response.status_code == 200:
     print("✅ yayyyyyyy:", response.json())
 else:
     print("❌ screw u:", response.status_code, response.text)
+
